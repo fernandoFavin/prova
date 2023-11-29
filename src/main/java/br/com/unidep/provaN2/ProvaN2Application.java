3a98b59d25afad42d0cb94941ec090cb877513ef
fernandoFavin <fernandoluizfavin@gmail.com>
2023-11-29 19:47:47 -0300

    Comitando

---
+++ src/main/java/br/com/unidep/provaN2/ProvaN2Application.java
@@ -0,0 +1,13 @@
+package br.com.unidep.provaN2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaN2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaN2Application.class, args);
+	}
+
+}
